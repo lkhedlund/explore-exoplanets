33a023b5a08b443ca7a7ab73b45ff9dd296447d0
Lars Hedlund <lkhedlund@gmail.com>
2015-12-15 17:39:02 -0800

    Starmap data loaded from ORM

--- kepler_exoplanets/views.py
+++ kepler_exoplanets/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Planet, Star
 
 def index(request):
     return render(request, 'main/index.html')
 
 def starmap(request):
-    return render(request, 'starmap/starmap.html')
+    stars = Star.objects.all().order_by('pk')
+    return render(request, 'starmap/starmap.html', {
+        'stars': stars
+    })
 
 def planet(request):
     return render(request, 'planets/planet.html')

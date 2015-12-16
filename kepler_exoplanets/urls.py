50ca5c0d2fd9bf50c30852fe3510b20724eb47b7
an1tahung <an1tahung@users.noreply.github.com>
2015-12-15 18:10:26 -0800

    Change gas giant material from mesh to shader

--- kepler_exoplanets/urls.py
+++ kepler_exoplanets/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^starmap/$', views.starmap, name='starmap'),
     url(r'^planet/$', views.planet, name='planet'),
+    url(r'^planet/gas_planet/$', views.gas_planet, name='planet'),
 ]

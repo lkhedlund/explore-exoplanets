8f77234f24eb64b0e57aa9226d0e74830275cd8d
Lars Hedlund <lkhedlund@gmail.com>
2015-12-18 11:36:42 -0800

    Star route added

--- kepler_exoplanets/urls.py
+++ kepler_exoplanets/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
     url(r'^starmap/$', views.starmap, name='starmap'),
-    url(r'stellar_system/(?P<star_id>[0-9]+)/$', views.stellar_system, name='stellar_system'),
+    url(r'stellar_system/(?P<system_id>[0-9]+)/$', views.stellar_system, name='stellar_system'),
     url(r'planet/(?P<planet_id>[0-9]+)/$', views.planet, name='planet'),
+    url(r'star/(?P<star_id>[0-9]+)/$', views.star, name='star'),
     # Testing Planets
     url(r'^planet/gas_planet/$', views.gas_planet, name='gas_planet'),
     url(r'^planet/test_planet/$', views.test_planet, name='test_planet'),

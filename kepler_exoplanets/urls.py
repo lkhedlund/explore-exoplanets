3aa8f9055b6bbeead95472c2527ebe2c1b7f674e
Lars Hedlund <lkhedlund@gmail.com>
2015-12-29 08:55:52 -0800

    Cleaned up bugs

--- kepler_exoplanets/urls.py
+++ kepler_exoplanets/urls.py
@@ -9,13 +9,4 @@ urlpatterns = [
     url(r'stellar_system/(?P<system_id>[0-9]+)/$', views.stellar_system, name='stellar_system'),
     url(r'planet/(?P<planet_id>[0-9]+)/$', views.planet, name='planet'),
     url(r'star/(?P<star_id>[0-9]+)/$', views.star, name='star'),
-<<<<<<< HEAD
-=======
-    # Testing Planets
-    url(r'^planet/redgas_planet/$', views.redgas_planet, name='redgas_planet'),
-    url(r'^planet/bluegas_planet/$', views.bluegas_planet, name='bluegas_planet'),
-    url(r'^planet/habitable_planet/$', views.habitable_planet, name='habitable_planet'),
-    url(r'^planet/rockyplanet/$', views.rocky_planet, name='rockyplanet'),
-    url(r'^stars/kepler_104/$', views.test_star, name='kepler104')
->>>>>>> 609442543caa17366f4e927ca8cf65e7edeb292e
 ]

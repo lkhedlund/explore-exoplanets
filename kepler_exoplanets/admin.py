65b7437369d7a6c2d10f147c2a6f700ee4817cf0
Lars Hedlund <lkhedlund@gmail.com>
2015-12-29 16:22:22 -0800

    Added back old files as backup for now

---
+++ kepler_exoplanets/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Planet, Star
+
+admin.site.register(Planet)
+admin.site.register(Star)
---
+++ kepler_js/static/js/planets/gas.js
@@ -0,0 +1,62 @@
+$(function() {
+  var container, stats;
+  var camera, scene, renderer, controls;
+  var fov = 30;
+  var texloader = new THREE.TextureLoader();
+  var explocolor = texloader.load('/static/images/redgas.png');
+  var start = Date.now()
+
+  init();
+
+  function init(){
+    //set up scene and camera
+    container = document.getElementById( 'space_container' );
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({alpha: true});
+    renderer.setClearColor( 0xffffff, 0);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera = new THREE.PerspectiveCamera(fov, window.innerWidth/ window.innerHeight, 0.1, 10000);
+    camera.position.z = 100;
+    camera.target = new THREE.Vector3( 0, 0, 0 );
+    scene.add( camera );
+    //move the camera around
+    controls = new THREE.OrbitControls( camera );
+    controls.minDistance = 100;
+    controls.maxDistance = 2000;
+    //light
+    var amlight = new THREE.AmbientLight( 0x888888 )
+    scene.add( amlight );
+    var dirlight = new THREE.DirectionalLight( 0xcccccc, 1 )
+    dirlight.position.set(10,3,5)
+    scene.add( dirlight );
+
+    //gasplanet
+    planetmaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        tExplosion: {
+          type: "t",
+          value: explocolor
+        },
+        time: { // float initialized to 0
+          type: "f",
+          value: 0.0
+        }
+      },
+      vertexShader: $.getScript("/script/js/planets/_gasvertex.js"),
+      fragmentShader: $.getScript("/script/js/planets/_gasfragment.js"),
+    });
+    var geometry = new THREE.IcosahedronGeometry( 20, 5 );
+    var gasplanet = new THREE.Mesh( geometry, planetmaterial );
+    scene.add(gasplanet);
+
+    container.appendChild( renderer.domElement );
+    animate();
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+    planetmaterial.uniforms[ 'time' ].value = .00025 * ( Date.now() - start );
+    controls.update();
+    renderer.render( scene, camera );
+  }
+});

4f4026cba1e39aac306defdb0031c184bef9adb2
Fabien Albi <fabien.albi@icloud.com>
2016-01-05 16:30:45 -0800

    Tranfered html scripts to js only files

--- kepler_js/public/js/starmap/starmap.js
+++ kepler_js/public/js/starmap/starmap.js
@@ -1,29 +1,35 @@
 var starmap = function(keplerData) {
+
   var controls, camera, scene, renderer, container, intersects;
 
+  // Load stars glow texture
+  var texloader = new THREE.TextureLoader();
+  var glow = texloader.load( '../static/images/glow.png' );
+
   // Set up canvas dimensions
-  var width = window.innerWidth;
+  var width = 1000;
   var height = window.innerHeight;
 
   // Set up tools for hover and click events
   var raycaster = new THREE.Raycaster();
   var mouse = new THREE.Vector2(), INTERSECTED;
 
-  // Create the starmap
-  start(keplerData);
+  // Create the starmap   
+  start();
 
-  function start(keplerData){
+  function start(){
+
     //Scene creation
     scene = new THREE.Scene();
-    container = document.getElementById('space_container');
+    container = document.getElementById( 'space_container' );
 
     // PerspectiveCamera creation and Set up. Parameters
     // 1. FOV – We’re using 45 degrees for our field of view.
     // 2. Aspect – We’re simply dividing the browser width and height to get an aspect ratio.
     // 3. Near – This is the distance at which the camera will start rendering scene objects.
     // 4. Far – Anything beyond this distance will not be rendered. Perhaps more commonly known as the draw distance.
-    camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 10000);
-    camera.position.set(0, 0, 2500);
+    camera = new THREE.PerspectiveCamera(75, width/height, 50, 10000);
+    camera.position.set(0, 0, 2600);
     scene.add(camera);
 
     // Mouse controls - Zoom only for skybox and starmap
@@ -32,8 +38,7 @@ var starmap = function(keplerData) {
     controls.maxDistance = 2600;
     controls.enableRotate = false;
     controls.enablePan = false;
-    skybox_controls.minDistance = 200;
-    skybox_controls.maxDistance = 2600;
+    skybox_controls.minDistance = 1000;
     skybox_controls.enableRotate = false;
     skybox_controls.enablePan = false;
 
@@ -47,26 +52,33 @@ var starmap = function(keplerData) {
     // Create the WebGL renderer and append it to the DOM bia the body element
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(width, height);
-    // renderer.setClearColor( 0x000000, 0 );
     container.appendChild(renderer.domElement);
     renderer.render(scene,camera);
 
-    // Add 300 spheres to the scene. Random size, random positioning
+    // Add all the stars to the scene.
     var geometry, material, star;
 
+    {% for star in stars %}
+      createStar({{star.stellar_temp}}, {{star.right_ascension}}, {{star.declination}}, {{star.light_years_dist}});
+    {% endfor %}
+
     function createStar (temp, ra, declination, ly) {
+      color = star_color(temp);
       geometry = new THREE.SphereGeometry(1, 15, 15);
-      material = new THREE.MeshBasicMaterial({color: star_color(temp), wireframe:false});
+      material = new THREE.MeshBasicMaterial({color: color, wireframe:false});
       star = new THREE.Mesh(geometry, material);
       scene.add(star);
       star.position.set((ra - 290) * 5 * (ly / 1000), (declination - 45) * 5 * (ly / 1000),ly - 500);
+    
+      // Star glow (using sprites)
+      var spriteMaterial = new THREE.SpriteMaterial({ 
+        map: glow, 
+        color: color, transparent: false, blending: THREE.AdditiveBlending
+      });
+      sprite = new THREE.Sprite( spriteMaterial );
+      sprite.scale.set(8, 8, 8);
+      star.add(sprite);        
     }
-
-    // Load star data from JSON file
-    for (var i = 0; i < keplerData.length; i++) {
-      var star = keplerData[i]["fields"];
-      createStar(star.stellar_temp, star.right_ascension, star.declination, star.light_years_dist);
-    };
 
     animate();
   }
@@ -104,7 +116,6 @@ var starmap = function(keplerData) {
   function onDocumentMouseClick() {
     event.preventDefault();
     grabPlanets();
-    console.log(camera.position.z);
     if (intersects.length > 0) {
       clickedPlanet = intersects[0].object;
       x = clickedPlanet.position.x;
@@ -112,7 +123,6 @@ var starmap = function(keplerData) {
       z = clickedPlanet.position.z;
       alert('Star coordinates:' + '\nx: ' + x.toFixed(2) + '\ny: ' + y.toFixed(2) + '\nz: ' + z.toFixed(2));
     }
-
   }
 
   // Create an event listener that resizes the renderer with the browser window.
@@ -122,15 +132,16 @@ var starmap = function(keplerData) {
     renderer.setSize(width, height);
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
-    render();
   });
 
   // Deactivate trackball controlls when interacting with the panel
-  $('.container').on('mousedown', function() {
-    controls.enabled = true;
+  $('.sidebar').on('mouseenter', function() {
+    controls.enabled = false;
+    skybox_controls.enabled = false;
   });
-  $('.container').on('mouseup', function() {
+  $('.sidebar').on('mouseleave', function() {
     controls.enabled = true;
+    skybox_controls.enabled = true;
   });
 
   // Stars are colored based on their temperature
@@ -168,14 +179,13 @@ var starmap = function(keplerData) {
 
   // Button on sidebar to reset camera
   $('#camera-reset').on('click', function() {
-    camera.position.set(0, 0, 500);
-    document.querySelector('#star-slider').value = 500
-    document.querySelector('#dist').value = 500
+    camera.position.set(0, 0, controls.maxDistance);
+    document.querySelector('#star-slider').value = controls.maxDistance;
+    document.querySelector('#dist').value = controls.maxDistance;
   });
 
   // Slider on sidebar to move forwards/backwards
   $('#star-slider').on('click', function() {
     camera.position.set(0, 0, dist.value);
   });
-
-};
+});
--- kepler_js/public/js/stellar_system/stellar_system.js
+++ kepler_js/public/js/stellar_system/stellar_system.js
@@ -1,20 +1,22 @@
 $(function() {
-  var control, camera, scene, renderer, container, intersects;
+  var controls, camera, scene, renderer, container, intersects;
+  circles = [];
 
   // Set up canvas dimensions
-  var width = window.innerWidth;
-  var height = window.innerHeight - 45;
+  var width = 1000;
+  var height = window.innerHeight;
 
   // Set up planets/sun textures
   var texloader = new THREE.TextureLoader();
-  var explocolor = texloader.load('../images/star.png');
-  var texture = texloader.load('../images/plutomap2k.jpg');
-  var bump = texloader.load('../images/plutobump2k.jpg');
+  var explocolor = texloader.load('/static/images/star.png');
+  var gascolor = texloader.load('/static/images/redgas.png');
+  var texture = texloader.load('/static/images/plutomap2k.jpg');
+  var bump = texloader.load('/static/images/plutobump2k.jpg');
 
   // Set up tools for hover and click events
   var raycaster = new THREE.Raycaster();
   var mouse = new THREE.Vector2(), INTERSECTED;
-
+  
   start();
 
   function start(){
@@ -27,9 +29,15 @@ $(function() {
     // 2. Aspect – We’re simply dividing the browser width and height to get an aspect ratio.
     // 3. Near – This is the distance at which the camera will start rendering scene objects.
     // 4. Far – Anything beyond this distance will not be rendered. Perhaps more commonly known as the draw distance.
-    camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 3000);
-    camera.position.set(0, - 50, 75);
+    camera = new THREE.PerspectiveCamera(75, width/height, 50, 1000);
+    camera.position.set(0, 0, 100);
     scene.add(camera);
+
+    // Set up camera controls (using Orbit)
+    controls = new THREE.OrbitControls( camera );
+    controls.rotateSpeed = 1;
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
 
     // Create the WebGL renderer and append it to the DOM bia the body element
     renderer = new THREE.WebGLRenderer({alpha: true});
@@ -37,118 +45,148 @@ $(function() {
     renderer.setSize(width, height);
     container.appendChild(renderer.domElement);
 
-    // Create and add lights to the scene
-    var amlight = new THREE.AmbientLight( 0x888888 )
-    scene.add( amlight );
-    var dirlight = new THREE.DirectionalLight( 0xcccccc, 1 )
-    dirlight.position.set(10,3,5)
-    scene.add( dirlight );
-
     // Create and add the star to the stellar system
     var geometry = new THREE.IcosahedronGeometry( 14, 5 );
     starmaterial = new THREE.ShaderMaterial({
-      uniforms: {
+      uniforms: { 
         tExplosion: {
-          type: "t",
+          type: "t", 
           value: explocolor
         },
         time: { // float initialized to 0
-          type: "f",
-          value: 0.0
+          type: "f", 
+          value: 0.0 
         }
       },
 
       vertexShader: document.getElementById( 'star-vertexShader' ).textContent,
       fragmentShader: document.getElementById( 'star-fragmentShader' ).textContent
     });
-    var star = new THREE.Mesh(geometry, starmaterial);
-    scene.add(star);
+    star = new THREE.Mesh(geometry, starmaterial);
+    scene.add( star );
+    pointLight = new THREE.PointLight( 0xffffff );
+    scene.add( pointLight );
+    pointLight.add( star );
 
-    // create and add three planets to the stellar system
+    // Circle
+    var circleSegments = 64;
+    var circleMaterial = new THREE.LineBasicMaterial( { color: 0xF0C400 } );
+
+    // Create and add three planets to the stellar system
+    //PLANET 1
     geometry = new THREE.SphereGeometry(3.08, 15, 15);
     var material = new THREE.MeshPhongMaterial({ map: texture, bumpMap: bump, bumpScale: 1  });
-    var planet1 = new THREE.Mesh(geometry, material);
+    planet1 = new THREE.Mesh(geometry, material);
     scene.add(planet1);
     planet1.position.set(0, 18, 0);
+    // CIRCLE 1
+    geometry = new THREE.CircleGeometry( 18, circleSegments );
+    geometry.vertices.shift();
+    var circle = new THREE.Line(geometry,  circleMaterial);
+    circles.push(circle);
+    scene.add(circle);
 
-    geometry = new THREE.SphereGeometry(3.06, 15, 15);
-    material = new THREE.MeshPhongMaterial({ map: texture, bumpMap: bump, bumpScale: 1  });
-    var planet2 = new THREE.Mesh(geometry, material);
+    //PLANET 2
+    planetmaterial = new THREE.ShaderMaterial({
+      uniforms: { 
+        tExplosion: {
+          type: "t", 
+          value: gascolor
+        },
+        time: { // float initialized to 0
+          type: "f", 
+          value: 0.0 
+        }
+      },
+    vertexShader: document.getElementById( 'gas-vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'gas-fragmentShader' ).textContent
+    });
+    geometry = new THREE.IcosahedronGeometry( 3.16, 5 );
+    planet2 = new THREE.Mesh( geometry, planetmaterial );
     scene.add(planet2);
     planet2.position.set(0, 30, 0);
+    // CIRCLE 2
+    geometry = new THREE.CircleGeometry( 30, circleSegments );
+    geometry.vertices.shift();
+    circle = new THREE.Line(geometry,  circleMaterial);
+    circles.push(circle);
+    scene.add(circle);
 
-    geometry = new THREE.SphereGeometry(3.41, 15, 15);
-    material = new THREE.MeshPhongMaterial({ map: texture, bumpMap: bump, bumpScale: 1  });
-    var planet3 = new THREE.Mesh(geometry, material);
+    // PLANET 3
+    material = new THREE.MeshPhongMaterial({ map: texture, bumpMap: bump, bumpScale: 0.02 });
+    geometry = new THREE.SphereGeometry( 3.4, 15, 15 );
+    planet3 = new THREE.Mesh( geometry, material );
     scene.add(planet3);
     planet3.position.set(0, 50, 0);
+    //CIRCLE 3 
+    geometry = new THREE.CircleGeometry( 50, circleSegments );
+    geometry.vertices.shift();
+    circle = new THREE.Line( geometry,  circleMaterial );
+    circles.push(circle);
+    scene.add(circle);
+      
+    t = 0;
+    y1 = planet1.position.y;
+    y2 = planet2.position.y;
+    y3 = planet3.position.y;
+    x = planet1.position.x;
+    launch = Date.now();
 
-    var t = 0;
-    var y1 = planet1.position.y;
-    var y2 = planet2.position.y;
-    var y3 = planet3.position.y;
-    var x = planet1.position.x;
+    animate();
 
-    function render() {
-      var launch = Date.now();
-      requestAnimationFrame(render);
-      starmaterial.uniforms[ 'time' ].value = .00025 * ( Date.now() - launch );
-      t += 0.01;
-      star.rotation.y += 0.005;
-      planet1.rotation.y += 0.03;
-      planet2.rotation.y += 0.04;
-      planet3.rotation.y += 0.05;
+  }
 
-      planet1.position.y = y1 * Math.cos(t) + x * Math.sin(t);
-      planet1.position.x = x * Math.cos(t) - y1 * Math.sin(t);
+  function animate() {
+    requestAnimationFrame(animate);
+    starmaterial.uniforms[ 'time' ].value = .00025 * ( Date.now() - launch );
+    t += 0.01;
+    star.rotation.z += 0.005;
+    planet1.rotation.z += 0.03;
+    planet2.rotation.z += 0.04;
+    planet3.rotation.z += 0.05;
 
-      planet2.position.y = y2 * Math.cos(t * 2) + x * Math.sin(t * 2);
-      planet2.position.x = x * Math.cos(t * 2) - y2 * Math.sin(t * 2);
+    planet1.position.y = y1 * Math.cos(t) + x * Math.sin(t);
+    planet1.position.x = x * Math.cos(t) - y1 * Math.sin(t);
 
-      planet3.position.y = y3 * Math.cos(t * 1.5) + x * Math.sin(t * 1.5);
-      planet3.position.x = x * Math.cos(t * 1.5) - y3 * Math.sin(t * 1.5);
+    planet2.position.y = y2 * Math.cos(t * 0.5) + x * Math.sin(t * 0.5);
+    planet2.position.x = x * Math.cos(t * 0.5) - y2 * Math.sin(t * 0.5);
 
-      var material = new THREE.MeshBasicMaterial({
-        color: 0x0000ff,
-        wireframe: true
-      });
+    planet3.position.y = y3 * Math.cos(t * 1.2) + x * Math.sin(t * 1.2);
+    planet3.position.x = x * Math.cos(t * 1.2) - y3 * Math.sin(t * 1.2);
 
-      var segments = 64;
-      material = new THREE.LineBasicMaterial( { color: 0xF0C400 } ),
-
-      geometry = new THREE.CircleGeometry( 18, segments );
-      geometry.vertices.shift();
-      var circle = new THREE.Line(geometry, material);
-      scene.add(circle);
-
-      geometry = new THREE.CircleGeometry( 30, segments );
-      geometry.vertices.shift();
-      var circle = new THREE.Line(geometry, material);
-      scene.add(circle);
-
-      geometry = new THREE.CircleGeometry( 50, segments );
-      geometry.vertices.shift();
-      var circle = new THREE.Line(geometry, material);
-      scene.add(circle);
-
-      controls = new THREE.OrbitControls( camera );
-
-      renderer.render(scene, camera);
-    }
-
-    render();
+    renderer.render(scene, camera);
+    controls.update();
   }
 
   // Make the planets clickable (for now, it alerts the coordinates)
-  document.addEventListener('click', onDocumentMouseClick, false);
+  document.addEventListener('dblclick', onDocumentMouseClick, false);
   function onDocumentMouseClick() {
     event.preventDefault();
     grabPlanets();
-
     if (intersects.length > 0) {
       alert(intersects[0].object);
     }
+    if (intersects.length == 0) {
+      console.log(camera.position);
+      if (camera.position.z == 75) { camera.position.set(0, 75, 0); }
+      else if (Math.round(camera.position.y) == 75) { camera.position.set(0, -60, 60); }
+      else { camera.position.set(0, 0, 75); }
+    }
+
   }
+
+  // Deactivate orbit controls when interacting with the panel
+  $('.sidebar').on('mouseenter', function() {
+    controls.enabled = false;
+    skybox_controls.enabled = false;
+    circles.map(function(circle){circle.visible = false});
+  });
+
+  $('.sidebar').on('mouseleave', function() {
+    controls.enabled = true;
+    skybox_controls.enabled = true;
+    circles.map(function(circle){circle.visible = true});
+  });
 
   // Register the targeted planet with click events
   function grabPlanets() {

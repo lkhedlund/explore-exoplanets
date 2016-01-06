d95177696de56d3f6556852984c4632e9989f68f
Fabien Albi <fabien.albi@icloud.com>
2016-01-06 15:23:19 -0800

    preparing stellar system template

--- kepler_js/public/js/stellar_system/stellar_system.js
+++ kepler_js/public/js/stellar_system/stellar_system.js
@@ -6,13 +6,6 @@ $(function() {
   var width = 1000;
   var height = window.innerHeight;
 
-  // Set up planets/sun textures
-  var texloader = new THREE.TextureLoader();
-  var explocolor = texloader.load('/static/images/star.png');
-  var gascolor = texloader.load('/static/images/redgas.png');
-  var texture = texloader.load('/static/images/rocky.jpg');
-  var bump = texloader.load('/static/images/rockybump.jpg');
-
   // Set up tools for hover and click events
   var raycaster = new THREE.Raycaster();
   var mouse = new THREE.Vector2(), INTERSECTED;
@@ -46,12 +39,58 @@ $(function() {
     container.appendChild(renderer.domElement);
 
     // Create and add the star to the stellar system
-    var geometry = new THREE.IcosahedronGeometry( 14, 5 );
+    addStar(star);
+
+    // Circle
+    var circleSegments = 64;
+    var circleMaterial = new THREE.LineBasicMaterial( { color: 0xF0C400 } );
+
+    // Create and add three planets to the stellar system
+    for (planet where planet.star = star.pk in planets) {
+      selectPlanet(planet);
+    }
+      
+
+  //   t = 0;
+  //   y1 = planet1.position.y;
+  //   y2 = planet2.position.y;
+  //   y3 = planet3.position.y;
+  //   x = planet1.position.x;
+  //   launch = Date.now();
+  //   animate();
+
+  // }
+
+  // function animate() {
+  //   requestAnimationFrame(animate);
+  //   starmaterial.uniforms[ 'time' ].value = .00025 * ( Date.now() - launch );
+  //   t += 0.01;
+  //   star.rotation.z += 0.005;
+  //   planet1.rotation.z += 0.03;
+  //   planet2.rotation.z += 0.04;
+  //   planet3.rotation.z += 0.05;
+
+  //   planet1.position.y = y1 * Math.cos(t) + x * Math.sin(t);
+  //   planet1.position.x = x * Math.cos(t) - y1 * Math.sin(t);
+
+  //   planet2.position.y = y2 * Math.cos(t * 0.5) + x * Math.sin(t * 0.5);
+  //   planet2.position.x = x * Math.cos(t * 0.5) - y2 * Math.sin(t * 0.5);
+
+  //   planet3.position.y = y3 * Math.cos(t * 1.2) + x * Math.sin(t * 1.2);
+  //   planet3.position.x = x * Math.cos(t * 1.2) - y3 * Math.sin(t * 1.2);
+
+  //   renderer.render(scene, camera);
+  //   controls.update();
+  // }
+
+  // add the star
+  function addStar(star) {
+    var geometry = new THREE.IcosahedronGeometry( star.stellar_radius * 10, 5 );
     starmaterial = new THREE.ShaderMaterial({
       uniforms: { 
         tExplosion: {
           type: "t", 
-          value: explocolor
+          value: Textures.sunexplocolor
         },
         time: { // float initialized to 0
           type: "f", 
@@ -62,38 +101,40 @@ $(function() {
       vertexShader: document.getElementById( 'star-vertexShader' ).textContent,
       fragmentShader: document.getElementById( 'star-fragmentShader' ).textContent
     });
-    star = new THREE.Mesh(geometry, starmaterial);
+    star = new THREE.Mesh( geometry, starmaterial );
     scene.add( star );
     pointLight = new THREE.PointLight( 0xffffff );
     scene.add( pointLight );
     pointLight.add( star );
+  }
 
-    // Circle
-    var circleSegments = 64;
-    var circleMaterial = new THREE.LineBasicMaterial( { color: 0xF0C400 } );
+  // select which type of planet to add
+  function selectPlanet(planet) {
+    if (planet.planet_radius > 4) {
+      addRed(planet);
+    }
+    else if (planet.planet_radius > 2) {
+      addBlue(planet);
+    }
+    else {
+      if (planet.surface_temp > 273 && planet.surface_temp < 373) {
+        addHabitable(planet);
+      }
+      else { 
+        addRocky(planet); 
+      }
+    }
+  }
 
-    // Create and add three planets to the stellar system
-    //PLANET 1
-    geometry = new THREE.SphereGeometry(3.08, 15, 15);
-    var material = new THREE.MeshPhongMaterial({ map: texture, bumpMap: bump, bumpScale: 1  });
-    planet1 = new THREE.Mesh(geometry, material);
-    scene.add(planet1);
-    planet1.position.set(0, 18, 0);
-    // CIRCLE 1
-    geometry = new THREE.CircleGeometry( 18, circleSegments );
-    geometry.vertices.shift();
-    var circle = new THREE.Line(geometry,  circleMaterial);
-    circles.push(circle);
-    scene.add(circle);
-
-    //PLANET 2
-    planetmaterial = new THREE.ShaderMaterial({
+  // add Red Gas Planet
+  function addRed(planet) {
+    redMaterial = new THREE.ShaderMaterial({
       uniforms: { 
         tExplosion: {
           type: "t", 
-          value: gascolor
+          value: Textures.redexplocolor
         },
-        time: { // float initialized to 0
+        time: {
           type: "f", 
           value: 0.0 
         }
@@ -101,61 +142,63 @@ $(function() {
     vertexShader: document.getElementById( 'gas-vertexShader' ).textContent,
     fragmentShader: document.getElementById( 'gas-fragmentShader' ).textContent
     });
-    geometry = new THREE.IcosahedronGeometry( 3.16, 5 );
-    planet2 = new THREE.Mesh( geometry, planetmaterial );
-    scene.add(planet2);
-    planet2.position.set(0, 30, 0);
-    // CIRCLE 2
-    geometry = new THREE.CircleGeometry( 30, circleSegments );
+    geometry = new THREE.IcosahedronGeometry( planet.planet_radius, 5 );
+    planet = new THREE.Mesh( geometry, redMaterial );
+    scene.add(planet);
+    planet.position.set(0, planet.semimajor_axis * 100, 0);
+    addCircle(planet.semimajor_axis * 100);
+  }
+
+  // add Blue Gas Planet
+  function addBlue(planet) {
+    blueMaterial = new THREE.ShaderMaterial({
+      uniforms: { 
+        tExplosion: {
+          type: "t", 
+          value: Textures.blueexplocolor
+        },
+        time: {
+          type: "f", 
+          value: 0.0 
+        }
+      },
+    vertexShader: document.getElementById( 'gas-vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'gas-fragmentShader' ).textContent
+    });
+    geometry = new THREE.IcosahedronGeometry( planet.planet_radius, 5 );
+    planet = new THREE.Mesh( geometry, blueMaterial );
+    scene.add(planet);
+    planet.position.set(0, planet.semimajor_axis * 100, 0);
+    addCircle(planet.semimajor_axis * 100)
+  }
+
+  // add rocky planet
+  function addRocky(planet) {
+    geometry = new THREE.SphereGeometry(planet.planet_radius, 15, 15);
+    rockyMaterial = new THREE.MeshPhongMaterial({ map: Textures.rockytexture, bumpMap: Textures.rockybump, bumpScale: 1  });
+    planet1 = new THREE.Mesh(geometry, rockyMaterial);
+    scene.add(planet1);
+    planet1.position.set(0, planet.semimajor_axis * 100, 0);
+    addCircle(planet.semimajor_axis * 100)
+  }
+
+  // add habitable planet
+  function addHabitable(planet) {
+    geometry = new THREE.SphereGeometry(planet.planet_radius, 15, 15);
+    habitableMaterial = new THREE.MeshPhongMaterial({ map: Textures.habitabletexture, bumpMap: Textures.habitablebump, bumpScale: 1  });
+    planet1 = new THREE.Mesh(geometry, habitableMaterial);
+    scene.add(planet1);
+    planet1.position.set(0, planet.semimajor_axis * 100, 0);
+    addCircle(planet.semimajor_axis * 100)
+  }
+
+  // add a orbit circle to the planet
+  function addCircle(distance) {
+    geometry = new THREE.CircleGeometry( distance, circleSegments );
     geometry.vertices.shift();
     circle = new THREE.Line(geometry,  circleMaterial);
     circles.push(circle);
     scene.add(circle);
-
-    // PLANET 3
-    material = new THREE.MeshPhongMaterial({ map: texture, bumpMap: bump, bumpScale: 0.02 });
-    geometry = new THREE.SphereGeometry( 3.4, 15, 15 );
-    planet3 = new THREE.Mesh( geometry, material );
-    scene.add(planet3);
-    planet3.position.set(0, 50, 0);
-    //CIRCLE 3 
-    geometry = new THREE.CircleGeometry( 50, circleSegments );
-    geometry.vertices.shift();
-    circle = new THREE.Line( geometry,  circleMaterial );
-    circles.push(circle);
-    scene.add(circle);
-      
-    t = 0;
-    y1 = planet1.position.y;
-    y2 = planet2.position.y;
-    y3 = planet3.position.y;
-    x = planet1.position.x;
-    launch = Date.now();
-
-    animate();
-
-  }
-
-  function animate() {
-    requestAnimationFrame(animate);
-    starmaterial.uniforms[ 'time' ].value = .00025 * ( Date.now() - launch );
-    t += 0.01;
-    star.rotation.z += 0.005;
-    planet1.rotation.z += 0.03;
-    planet2.rotation.z += 0.04;
-    planet3.rotation.z += 0.05;
-
-    planet1.position.y = y1 * Math.cos(t) + x * Math.sin(t);
-    planet1.position.x = x * Math.cos(t) - y1 * Math.sin(t);
-
-    planet2.position.y = y2 * Math.cos(t * 0.5) + x * Math.sin(t * 0.5);
-    planet2.position.x = x * Math.cos(t * 0.5) - y2 * Math.sin(t * 0.5);
-
-    planet3.position.y = y3 * Math.cos(t * 1.2) + x * Math.sin(t * 1.2);
-    planet3.position.x = x * Math.cos(t * 1.2) - y3 * Math.sin(t * 1.2);
-
-    renderer.render(scene, camera);
-    controls.update();
   }
 
   // Make the planets clickable (for now, it alerts the coordinates)
@@ -172,10 +215,10 @@ $(function() {
       else if (Math.round(camera.position.y) == 75) { camera.position.set(0, -60, 60); }
       else { camera.position.set(0, 0, 75); }
     }
-
   }
 
   // Deactivate orbit controls when interacting with the panel
+  // Show/hide orbit circles
   $('.sidebar').on('mouseenter', function() {
     controls.enabled = false;
     skybox_controls.enabled = false;

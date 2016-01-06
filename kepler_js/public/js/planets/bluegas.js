eee31be90ac7e34afeffbce71bedc4decc6de2ca
Anita Hung <anitalhung@gmail.com>
2016-01-05 16:44:06 -0800

    Transferred html files in explore_exoplanets to js files in kepler_js

--- kepler_js/public/js/planets/bluegas.js
+++ kepler_js/public/js/planets/bluegas.js
@@ -1,9 +1,9 @@
 $(function() {
   var container, stats;
   var camera, scene, renderer, controls;
-  var fov = 30;
+  var fov = 25;
   var texloader = new THREE.TextureLoader();
-  var explocolor = texloader.load('../../images/redgas.png');
+  var blueexplocolor = texloader.load('/public/images/bluegas.png');
   var start = Date.now()
 
   init();
@@ -12,51 +12,58 @@ $(function() {
     //set up scene and camera
     container = document.getElementById( 'space_container' );
     scene = new THREE.Scene();
+    group = new THREE.Group();
     renderer = new THREE.WebGLRenderer({alpha: true});
     renderer.setClearColor( 0xffffff, 0);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera = new THREE.PerspectiveCamera(fov, window.innerWidth/ window.innerHeight, 0.1, 10000);
+    renderer.setSize(1000, window.innerHeight);
+    camera = new THREE.PerspectiveCamera(fov, 1000/ window.innerHeight, 50, 10000);
     camera.position.z = 100;
     camera.target = new THREE.Vector3( 0, 0, 0 );
     scene.add( camera );
+
     //move the camera around
     controls = new THREE.OrbitControls( camera );
     controls.minDistance = 100;
-    controls.maxDistance = 2000;
+    controls.maxDistance = 400;
+
     //light
     var amlight = new THREE.AmbientLight( 0x888888 )
     scene.add( amlight );
+
     var dirlight = new THREE.DirectionalLight( 0xcccccc, 1 )
     dirlight.position.set(10,3,5)
     scene.add( dirlight );
 
     //gasplanet
     planetmaterial = new THREE.ShaderMaterial({
-      uniforms: {
+      uniforms: { 
         tExplosion: {
-          type: "t",
-          value: explocolor
+          type: "t", 
+          value: blueexplocolor
         },
         time: { // float initialized to 0
-          type: "f",
-          value: 0.0
+          type: "f", 
+          value: 0.0 
         }
       },
-      vertexShader: $.getScript("/script/js/planets/_gasvertex.js"),
-      fragmentShader: $.getScript("/script/js/planets/_gasfragment.js"),
+      vertexShader: document.getElementById( 'gas-vertexShader' ).textContent,
+      fragmentShader: document.getElementById( 'gas-fragmentShader' ).textContent
     });
+
     var geometry = new THREE.IcosahedronGeometry( 20, 5 );
     var gasplanet = new THREE.Mesh( geometry, planetmaterial );
     scene.add(gasplanet);
 
+    
     container.appendChild( renderer.domElement );
     animate();
-  }
 
-  function animate() {
-    requestAnimationFrame(animate);
-    planetmaterial.uniforms[ 'time' ].value = .00025 * ( Date.now() - start );
-    controls.update();
-    renderer.render( scene, camera );
+    function animate() {
+      requestAnimationFrame(animate);
+      planetmaterial.uniforms[ 'time' ].value = .00025 * ( Date.now() - start );
+      gasplanet.rotateY(2/1000)
+      controls.update();
+      renderer.render( scene, camera );
+    }
   }
 });

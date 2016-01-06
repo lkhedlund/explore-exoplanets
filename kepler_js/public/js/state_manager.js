2009e61289026a372d31ad7e355df5ea9dcd0470
Lars Hedlund <lkhedlund@gmail.com>
2016-01-05 17:27:26 -0800

    One page rendering successfully

--- kepler_js/public/js/state_manager.js
+++ kepler_js/public/js/state_manager.js
@@ -9,7 +9,7 @@ $(function () {
   });
 
   // Call render on hash change
-  $(window)on('hashchange', function() {
+  $(window).on('hashchange', function() {
     render(window.location.hash);
   });
 
@@ -32,9 +32,21 @@ $(function () {
     }
   };
 
+  function renderStarmapPage(data) {
+    var page = $('.starmap');
+    page.addClass('visible');
+    starmap(data);
+  };
+
   function renderErrorPage() {
     var page = $('.error');
     page.addClass('visible');
-  }
+  };
+
+  // function addScript(src) {
+  //   var script = document.createElement('script');
+  //   script.setAttribute('src', src);
+  //   document.body.appendChild(script);
+  // }
 
 });

19b5167560754d7d66c9600a5203186099d9d5f5
Lars Hedlund <lkhedlund@gmail.com>
2016-01-13 10:06:56 -0800

    Returned main page to index

--- kepler_exoplanets/urls.py
+++ kepler_exoplanets/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^$', views.index, name='index'),
-    url(r'^$', views.starmap, name='starmap'),
+    url(r'^$', views.index, name='index'),
+    url(r'^starmap/$', views.starmap, name='starmap'),
     url(r'stellar_system/(?P<system_id>[0-9]+)/$', views.stellar_system, name='stellar_system'),
     url(r'planet/(?P<planet_id>[0-9]+)/$', views.planet, name='planet'),
     url(r'star/(?P<star_id>[0-9]+)/$', views.star, name='star'),

b629dc63c10c29a00cc13648a594628525f291b3
Fabien Albi <fabien.albi@icloud.com>
2016-01-12 18:23:40 -0800

    Made the skybox as a template

--- kepler_exoplanets/static/js/skybox.js
+++ kepler_exoplanets/static/js/skybox.js
@@ -1,73 +1,94 @@
+var skybox = function(type){ 
+
   var skybox_camera, scene, renderer,skybox, skybox_controls;
   var fov = 25;
   var texloader = new THREE.TextureLoader();
 
-  var sky_bk = texloader.load( '/static/images/skybox/bluenebulaBK.png' );
-  var sky_dn = texloader.load( '/static/images/skybox/bluenebulaDN.png' );
-  var sky_ft = texloader.load( '/static/images/skybox/bluenebulaFT.png' );
-  var sky_lf = texloader.load( '/static/images/skybox/bluenebulaLF.png' );
-  var sky_rt = texloader.load( '/static/images/skybox/bluenebulaRT.png' );
-  var sky_up = texloader.load( '/static/images/skybox/bluenebulaUP.png' );
+  if (type == 'index') {
+    var sky_bk = Textures.skybox_index_bk
+    var sky_dn = Textures.skybox_index_dn
+    var sky_ft = Textures.skybox_index_ft
+    var sky_lf = Textures.skybox_index_lf
+    var sky_rt = Textures.skybox_index_rt
+    var sky_up = Textures.skybox_index_up
+  }
+  else {
+    var sky_bk = Textures.skybox_classic_bk
+    var sky_dn = Textures.skybox_classic_dn
+    var sky_ft = Textures.skybox_classic_ft
+    var sky_lf = Textures.skybox_classic_lf
+    var sky_rt = Textures.skybox_classic_rt
+    var sky_up = Textures.skybox_classic_up
+  }
 
   $(function() {
 
-  init();
+    init();
 
-  function init(){
-    //set up scene and camera
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    //field_of_view, aspect_ratio, near_clip, far_clip
-    skybox_camera = new THREE.PerspectiveCamera(fov, window.innerWidth/ window.innerHeight, 50, 10000);
-    skybox_camera.position.set(0, 0, 3000);
-    skybox_camera.lookAt(scene.position);
-    scene.add( skybox_camera );
+    function init(){
+      //set up scene and camera
+      scene = new THREE.Scene();
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      //field_of_view, aspect_ratio, near_clip, far_clip
+      skybox_camera = new THREE.PerspectiveCamera(fov, window.innerWidth/ window.innerHeight, 50, 10000);
+      skybox_camera.position.set(0, 0, 3000);
+      skybox_camera.lookAt(scene.position);
+      scene.add( skybox_camera );
 
-    // helps keep track of skybox side when testing
-    // var axes = new THREE.AxisHelper(100);
-    // scene.add(axes);
+      // helps keep track of skybox side when testing
+      // var axes = new THREE.AxisHelper(100);
+      // scene.add(axes);
 
-    // camera movement
-    skybox_controls = new THREE.OrbitControls( skybox_camera );
-    skybox_controls.minDistance = 3000;
-    skybox_controls.maxDistance = 4000;
+      // camera movement
+      skybox_controls = new THREE.OrbitControls( skybox_camera );
+      skybox_controls.minDistance = 3000;
+      skybox_controls.maxDistance = 4000;
+      if (type == 'starmap') {
+        skybox_camera.position.set(0, 0, 4000);
+        skybox_controls.enableRotate = false;
+      }
 
-    //box sides
-    var materialArray = [];
-      materialArray.push(new THREE.MeshBasicMaterial( { map: sky_bk }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: sky_ft }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: sky_up }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: sky_dn }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: sky_rt }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: sky_lf }));
+      //box sides
+      var materialArray = [];
+        materialArray.push(new THREE.MeshBasicMaterial( { map: sky_bk }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: sky_ft }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: sky_up }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: sky_dn }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: sky_rt }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: sky_lf }));
 
-    for (var i = 0; i < 6; i++){
-       materialArray[i].side = THREE.BackSide;
+      for (var i = 0; i < 6; i++){
+         materialArray[i].side = THREE.BackSide;
+      }
+      var skyboxMaterial = new THREE.MeshFaceMaterial( materialArray );
+      var skyboxGeom = new THREE.CubeGeometry( 5000, 5000, 5000, 1, 1, 1 );
+      var skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
+      scene.add( skybox );
+
+      document.body.appendChild( renderer.domElement );
+      animate();
+
+      window.addEventListener('resize', function() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        renderer.setSize(width, height);
+        skybox_camera.aspect = width / height;
+        skybox_camera.updateProjectionMatrix();
+      });
+
+      function animate() {
+        requestAnimationFrame(animate);
+        if (type == 'index') {
+          skybox.rotateX(-0.5/1000); 
+        } else {
+          skybox.rotateY(-0.5/1000);
+        }
+        renderer.render( scene, skybox_camera );
+      }
+      window.skybox_camera = skybox_camera;
+      window.skybox = skybox;
+      window.skybox_controls = skybox_controls;
     }
-    var skyboxMaterial = new THREE.MeshFaceMaterial( materialArray );
-    var skyboxGeom = new THREE.CubeGeometry( 5000, 5000, 5000, 1, 1, 1 );
-    var skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
-    scene.add( skybox );
-
-    document.body.appendChild( renderer.domElement );
-    animate();
-
-    window.addEventListener('resize', function() {
-      width = window.innerWidth;
-      height = window.innerHeight;
-      renderer.setSize(width, height);
-      skybox_camera.aspect = width / height;
-      skybox_camera.updateProjectionMatrix();
-    });
-
-    function animate() {
-      requestAnimationFrame(animate);
-      skybox.rotateY(-0.5/1000);
-      renderer.render( scene, skybox_camera );
-    }
-    window.skybox_camera = skybox_camera;
-    window.skybox = skybox;
-    window.skybox_controls = skybox_controls;
-  }
-});
+  });
+};
--- kepler_exoplanets/static/js/textures.js
+++ kepler_exoplanets/static/js/textures.js
@@ -8,7 +8,19 @@ var Textures = (function() {
     habitabletexture:texloader.load('/static/images/habitableplanet.png'),
     habitablebump:texloader.load('/static/images/habitableplanetbump.png'),
     sunexplocolor:texloader.load('/static/images/star.png'),
-    glow:texloader.load( '/static/images/glow.png' )
+    glow:texloader.load( '/static/images/glow.png' ),
+    skybox_classic_bk: texloader.load( '/static/images/skybox/bluenebulaBK.png' ),
+    skybox_classic_dn: texloader.load( '/static/images/skybox/bluenebulaDN.png' ),    
+    skybox_classic_ft: texloader.load( '/static/images/skybox/bluenebulaFT.png' ),
+    skybox_classic_lf: texloader.load( '/static/images/skybox/bluenebulaLF.png' ),
+    skybox_classic_rt: texloader.load( '/static/images/skybox/bluenebulaRT.png' ),    
+    skybox_classic_up: texloader.load( '/static/images/skybox/bluenebulaUP.png' ),
+    skybox_index_bk: texloader.load( '/static/images/skybox/skyindexBK.png' ),
+    skybox_index_dn: texloader.load( '/static/images/skybox/skyindexDN.png' ),    
+    skybox_index_ft: texloader.load( '/static/images/skybox/skyindexFT.png' ),
+    skybox_index_lf: texloader.load( '/static/images/skybox/skyindexLF.png' ),
+    skybox_index_rt: texloader.load( '/static/images/skybox/skyindexRT.png' ),    
+    skybox_index_up: texloader.load( '/static/images/skybox/skyindexUP.png' ),
   }
   return api;
 })();
